b1f19eae9fe4f7e9cd1616a44d4e44d988149509
refazul <refazul.refat@yahoo.com>
2021-12-22 16:58:44 +0600

    some refactoring

---
+++ components/bonik/nav_json.js
@@ -0,0 +1,104 @@
+module.exports = 
+[
+    {
+        "title": "Fashion",
+        "type": "megamenu_large",
+        "children": [
+            {
+                "title": "Man Clothes",
+                "children": [
+                    { "title": "Shirt" },
+                    { "title": "T-Shirt" },
+                    { "title": "Pant" },
+                    { "title": "Underwear" },
+                ]
+            },
+            {
+                "title": "Accessories",
+                "children": [
+                    { "title": "Belt" },
+                    { "title": "Hat" },
+                    { "title": "Watches" },
+                    { "title": "Sunglasses" },
+                ]
+            },
+            {
+                "title": "Shoes",
+                "children": [
+                    { "title": "Sneakers" },
+                    { "title": "Sandals" },
+                    { "title": "Formal" },
+                    { "title": "Casual" },
+                ]
+            },
+            {
+                "title": "Bags",
+                "children": [
+                    { "title": "Backpack" },
+                    { "title": "Crossbody Bags" },
+                    { "title": "Side Bags" },
+                    { "title": "Slides" },
+                ]
+            },
+            {
+                "title": "Woman Clothes",
+                "children": [
+                    { "title": "Shirt" },
+                    { "title": "T-Shirt" },
+                    { "title": "Pant" },
+                    { "title": "Underwear" },
+                ]
+            },
+            {
+                "title": "Accessories",
+                "children": [
+                    { "title": "Belt" },
+                    { "title": "Hat" },
+                    { "title": "Watches" },
+                    { "title": "Sunglasses" },
+                ]
+            },
+            {
+                "title": "Shoes",
+                "children": [
+                    { "title": "Sneakers" },
+                    { "title": "Sandals" },
+                    { "title": "Formal" },
+                    { "title": "Casual" },
+                ]
+            },
+            {
+                "title": "Bags",
+                "children": [
+                    { "title": "Backpack" },
+                    { "title": "Crossbody Bags" },
+                    { "title": "Side Bags" },
+                    { "title": "Slides" },
+                ]
+            },
+        ],
+    },
+    {
+        "title": "Bikes",
+        "children": [
+            {
+                "title": "Man",
+                "children": [
+                    { "title": "Shirt" },
+                    { "title": "T-Shirt" },
+                    { "title": "Pant" },
+                    { "title": "Underwear" },
+                ]
+            },
+            {
+                "title": "Woman",
+            },
+            {
+                "title": "Baby Boy",
+            },
+            {
+                "title": "Baby Girl",
+            }
+        ]
+    }
+]
