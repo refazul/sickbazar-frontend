7c94245bcf63f3a116ffddadb28795543317b1e5
refazul <refazul.refat@yahoo.com>
2021-12-24 12:42:16 +0600

    body prototype

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Nav from '../components/bonik/nav'
+import Page from '../components/bonik/page'
 
 export default function Home() {
     return (
         <div>
-            <Nav></Nav>
+            <Page></Page>
         </div>
     )
 }
