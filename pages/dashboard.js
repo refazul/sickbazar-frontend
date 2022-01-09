956ddaccb37222605a3043f7cc1e62301935d3d1
refazul <refazul.refat@yahoo.com>
2022-01-09 16:20:11 +0600

    Multi Select Dropdown draft

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,18 @@
+import { Dropdown } from "../components/bonik/dropdown"
+
+const options = [
+    { title: "One", value: "1" },
+    { title: "Two", value: "2" },
+    { title: "Three", value: "3", selected: true },
+    { title: "Four", value: "4" },
+    { title: "Five", value: "5" },
+    { title: "Six", value: "6" },
+]
+
 export default function Dashboard() {
     return (
         <div>
-            Dashboard
+            <Dropdown opts={options}></Dropdown>
         </div>
     )
 }
