fb83cb37371faf4bb6407eb33aeaf4a82d22fa59
refazul <refazul.refat@yahoo.com>
2022-01-10 18:23:26 +0600

    Table component prototype

--- components/bonik/elements.js
+++ components/bonik/elements.js
@@ -210,3 +210,92 @@ export function ImageGallery() {
         </div>
     )
 }
+
+export function Table({ columns, children }) {
+    return (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    {
+                                        columns.map((column) => {
+                                            return (
+                                                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    {column.title}
+                                                </th>
+                                            )
+                                        })
+                                    }
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {children}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export function CellImageMultiLinel({ image, lines }) {
+    return (
+        <td className="px-6 py-4 whitespace-nowrap">
+            <div className="flex items-center">
+                <div className="flex-shrink-0 h-10 w-10">
+                    <img className="h-10 w-10 rounded-full" src={image} alt="" />
+                </div>
+                <div className="ml-4">
+                    {
+                        lines.map((line, index) => {
+                            if (index == 0) {
+                                return <div className="text-sm font-medium text-gray-900">{line}</div>
+                            }
+                            return <div className="text-sm text-gray-500">{line}</div>
+                        })
+                    }
+                </div>
+            </div>
+        </td>
+    )
+}
+export function CellMultiLine({ lines }) {
+    return (
+        <td className="px-6 py-4 whitespace-nowrap">
+            {
+                lines.map((line, index) => {
+                    if (index == 0) {
+                        return <div className="text-sm text-gray-900">{line}</div>
+                    }
+                    return <div className="text-sm text-gray-500">{line}</div>
+                })
+            }
+        </td>
+    )
+}
+export function CellColor({ text, textcolor, bgcolor }) {
+    return (
+        <td className="px-6 py-4 whitespace-nowrap">
+            <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-${bgcolor} text-${textcolor}}`}>
+                {text}
+            </span>
+        </td>
+    )
+}
+export function Cell({ text }) {
+    return (
+        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{text}</td>
+    )
+}
+export function CellLink({ text, link }) {
+    return (
+        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+            <a href={link} className="text-indigo-600 hover:text-indigo-900">
+                {text}
+            </a>
+        </td>
+    )
+}
--- pages/[entity]/[id].js
+++ pages/[entity]/[id].js
@@ -1,5 +1,5 @@
 import { readEntity } from '../../services/entity';
-import { Options, BreadCrumb, ImageGallery } from '../../components/bonik/elements';
+import { Options, BreadCrumb, ImageGallery, Table, Cell, CellMultiLine } from '../../components/bonik/elements';
 import React, { useState } from 'react';
 import { color_array, size_array } from '../../data';
 
@@ -9,53 +9,80 @@ export default function EntityDetail({ entity, object }) {
     }
     return (
         <div className="bg-white">
-            <div className="pt-6">
-                <BreadCrumb></BreadCrumb>
+            <div className="">
+                {
+                    entity == 'products' ? <div>
+                        <BreadCrumb></BreadCrumb>
+                        <ImageGallery></ImageGallery>
+                    </div> : <div></div>
+                }
 
-                <ImageGallery></ImageGallery>
 
-                <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
-                    <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8">
+                    <div className="lg:pr-8 pb-4">
                         <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
                             {object.title}
                         </h1>
                     </div>
 
-                    <Options options={{ color_array, size_array }} onClick={onAddToCart}></Options>
+                    {
+                        entity == 'products' ? <div>
+                            <Options options={{ color_array, size_array }} onClick={onAddToCart}></Options>
 
-                    <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                        <div>
-                            <h3 className="sr-only">Description</h3>
+                            <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                                <div>
+                                    <h3 className="sr-only">Description</h3>
 
-                            <div className="space-y-6">
-                                <p className="text-base text-gray-900">{object.description}</p>
+                                    <div className="space-y-6">
+                                        <p className="text-base text-gray-900">{object.description}</p>
+                                    </div>
+                                </div>
+
+                                <div className="mt-10">
+                                    <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+                                    <div className="mt-4">
+                                        <ul role="list" className="pl-4 list-disc text-sm space-y-2">
+                                            <li className="text-gray-400"><span className="text-gray-600">Hand cut and sewn locally</span></li>
+
+                                            <li className="text-gray-400"><span className="text-gray-600">Dyed with our proprietary colors</span></li>
+
+                                            <li className="text-gray-400"><span className="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
+
+                                            <li className="text-gray-400"><span className="text-gray-600">Ultra-soft 100% cotton</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div className="mt-10">
+                                    <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+                                    <div className="mt-4 space-y-6">
+                                        <p className="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </div> : <div></div>
+                    }
+                    {
+                        entity = 'attributes' ? <div>
+                            <Table columns={[{ title: "Title" }, { title: "Value" }, { title: "Color" }, { title: "Image" }]}>
+                             {
+                                object.options.map((option) => {
+                                    return (
+                                        <tr>
+                                            <Cell text={option.title}></Cell>
+                                            <Cell text={option.value}></Cell>
+                                            <Cell text={option.color}></Cell>
+                                            <Cell text={option.image}></Cell>
+                                        </tr>
+                                    )
+                                })
+                            }
+                            </Table>
+                        </div> : <div></div>
+                    }
 
-                        <div className="mt-10">
-                            <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-
-                            <div className="mt-4">
-                                <ul role="list" className="pl-4 list-disc text-sm space-y-2">
-                                    <li className="text-gray-400"><span className="text-gray-600">Hand cut and sewn locally</span></li>
-
-                                    <li className="text-gray-400"><span className="text-gray-600">Dyed with our proprietary colors</span></li>
-
-                                    <li className="text-gray-400"><span className="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
-
-                                    <li className="text-gray-400"><span className="text-gray-600">Ultra-soft 100% cotton</span></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="mt-10">
-                            <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-                            <div className="mt-4 space-y-6">
-                                <p className="text-sm text-gray-600">The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming &quot;Charcoal Gray&quot; limited release.</p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -69,8 +96,11 @@ export async function getServerSideProps(context) {
     const param = {};
     if (entity == 'products') {
         param.extra_fields = ['groupID', 'categoryIDs'];
+    } else if (entity == 'attributes') {
+        param.extra_fields = ['name', 'type', 'options{title, value, color, image}'];
     }
     props.object = await readEntity(entity, id, param);
+    console.log(props.object)
 
     return {
         props
